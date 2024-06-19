6bbcd7b256fb131f72ccc0e07792541ae48e554b
Paul K <spkim0921@gmail.com>
2024-06-19 16:44:08 -0700

    38th commit
    building west vancouver popup in home page index.html

---
+++ script/popup.js
@@ -0,0 +1,13 @@
+
+const popup         = document.getElementById('pop-up');    
+
+// const closeButton  = document.getElementById("close-pop-up");
+const okButton =  document.getElementById("ok-button");
+
+// closeButton.addEventListener('click', function (){
+//     popup.style.display = "none";
+// });
+
+okButton.addEventListener('click', function(){    
+    popup.style.display = "none";
+});
